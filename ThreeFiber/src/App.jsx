c8d67000a9a2fe927879b49b3ff89dedc150d332
sunnyWind <1749790593@qq.com>
2021-11-12 01:22:37 +0800

    feat:three add text

--- ThreeFiber/src/App.jsx
+++ ThreeFiber/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ThreeTest from './components/ThreeTest'
-
+import './App.css'
 export default function App() {
   return (
     <div>
-      <ThreeTest/>
+      <ThreeTest />
+      <div className="text-container">
+        <h1 >Make time for life ,</h1>
+        <h1 >or life won't make time .</h1>
+      </div>
     </div>
   )
 }
--- ThreeFiber/src/components/ThreeTest/index.jsx
+++ ThreeFiber/src/components/ThreeTest/index.jsx
@@ -1,22 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Stars, OrbitControls, Plane } from '@react-three/drei'
+import { Stars, OrbitControls, Plane, useNormalTexture } from '@react-three/drei'
 import { useBox } from '@react-three/cannon'
+import { MeshNormalMaterial, MeshToonMaterial } from 'three';
 const ThreeTest = () => {
 
     return (
         <div className="canvas-container"  >
             <Canvas
                 style={{ height: '100vh' }}>
-                {/* <Foo /> */}
-                <OrbitControls />
-                <Stars />
-                <axesHelper />
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 15, 10]} angle={0.3} />
-                <Planet position={[-6, 3.5, -1]} />
-                <Planet position={[6.5, 4, -0.5]} />
-                <Planet position={[1, -3, 1]} />
+
+                <Suspense fallback={('loading...')}>
+                    {/* <Planet position={[-6, 3.5, -1]} color='#27282a' />
+                    <Planet position={[6.5, 4, -0.5]} color='#53607b' />
+                    <Planet position={[1, -3, 1]} color='#92411e' /> */}
+                </Suspense>
                 {/* <_Plane /> */}
             </Canvas>
         </div>
@@ -24,16 +22,27 @@ const ThreeTest = () => {
     );
 }
 
-function Foo() {
-    const set = useThree((state) => state.set)
-    set({ frameloop: 'demand' })
-    return null
-}
+{/* <OrbitControls />
+<Stars />
+<ambientLight intensity={0.5} />
+<spotLight position={[10, 15, 10]} angle={0.3} /> */}
 
 
 function Planet(props) {
     const [active, setActive] = useState(false)
     const [hover, setHover] = useState(false)
+    const {
+        color = 'orange',
+        position,
+    } = props
+
+    const [normalMap, _] = useNormalTexture(
+        25,
+        {
+            offset: [0, 0],
+            anisotropy: 8
+        }
+    )
     const mesh = useRef()
     useFrame(() => { mesh.current.rotation.z += 0.01 })
     return (
@@ -43,10 +52,11 @@ function Planet(props) {
             onClick={() => { setActive(!active) }}
             onPointerEnter={() => { setHover(true) }}
             onPointerOut={() => { setHover(false) }}
-            {...props}
+            position={position}
         >
             <sphereGeometry widthSegments={32} heightSegments={16} />
-            <meshStandardMaterial color={hover ? 'hotpink' : 'orange'} />
+            <meshStandardMaterial color={color} normalMap={normalMap} />
+            {/* color={hover ? 'hotpink' : 'orange'} */}
         </mesh>
     )
 }
@@ -73,4 +83,15 @@ function _Plane() {
         </mesh>
     )
 }
+
+
+
+function Foo() {
+    const set = useThree((state) => state.set)
+    set({ frameloop: 'demand' })
+    return null
+}
+
+
+
 export default ThreeTest;
